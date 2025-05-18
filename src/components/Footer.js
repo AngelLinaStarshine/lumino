d662f269b617ffcfa5de79c4c6f9663f396b05c8
AngelLinaStarshine <deniagaangelina@gmail.com>
2025-05-18 01:22:18 -0400

    Initial commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,11 +3,15 @@ import '../App.css';
 import fbIcon from '../assets/fb.png';
 import whatsappIcon from '../assets/phone.png';
 import instagramIcon from '../assets/insta.png';
+import linkedinIcon from '../assets/linkedin.png';
 
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="social-media-links">
+      <a href="https://www.linkedin.com/company/lumino-learn-academy/posts/?feedView=all" target="_blank" rel="noopener noreferrer">
+          <img src={linkedinIcon} alt="LinkedIn" className="social-icon linkedin-icon" />
+        </a>
         <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
           <img src={fbIcon} alt="Facebook" className="social-icon" />
         </a>
